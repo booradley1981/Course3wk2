3895acae5ebb173fc86c0d853f8f915605402d4f
Jude Maghirang <maghirjn@mdrstudio01.unos.corp>
2018-02-18 08:15:45 -0500

    Initial commit to repository

---
+++ tests/testthat.R
@@ -0,0 +1,6 @@
+library(testthat)
+library(Course3wk2)
+
+test_check("Course3wk2")
+
+
---
+++ tests/testthat/tests.R
@@ -0,0 +1,15 @@
+# test the make_filename helper function
+
+test_that("make_file",{
+  fnstr <- make_filename(2013)
+  expect_that(fnstr, is_a("character"))
+}
+)
+
+
+# test the fars_read_years function
+test_that("read_years",{
+  yeardf <- fars_read_years(2013)
+  expect_that(yeardf, is_a("list"))
+}
+)
